fa1a818315898b3dc3ad402d9a93741c7469bf69
APRO2010 <prasad.esarapu@rankuno.com>
2025-09-15 14:05:58 +0530

    added the library page changes

--- templates/libraryNew/libraryNew.js
+++ templates/libraryNew/libraryNew.js
@@ -4,15 +4,15 @@ import { buildArticleSchema } from '../../scripts/schema.js';
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
   const section = main.querySelector(':scope > div:nth-child(2)');
-  section.prepend(
-    buildBlock('social-media', { elems: [] }),
-  );
+  // section.prepend(
+  //   buildBlock('social-media', { elems: [] }),
+  // );
 
-  const additionalContentSection = document.createElement('div');
-  additionalContentSection.append(
-    buildBlock('tags-list', { elems: [] }),
-    buildBlock('related-articles', { elems: [] }),
-  );
+  // const additionalContentSection = document.createElement('div');
+  // additionalContentSection.append(
+  //   buildBlock('tags-list', { elems: [] }),
+  //   buildBlock('related-articles', { elems: [] }),
+  // );
 
   buildArticleSchema();
 }
