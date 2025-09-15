2c7b680180bf830d77e8785e3cb5c442a9b7dbb7
rajesh-abcam <rajesh.chithaluri@abcam.com>
2025-09-15 15:09:46 +0530

    Adding converter conditon (#1853)
    * Adding converter conditon
    
    * initalized for initial load
    
    * exporting const values
    
    ---------
    
    Co-authored-by: Rajesh Chithaluri <rajesh.chithaluri.abcam.com>

--- tools/actions/convert/src/converter-paths-config.js
+++ tools/actions/convert/src/converter-paths-config.js
@@ -5,14 +5,43 @@
   ::::::::::::::
   *
 */
+
+const template = document.querySelector('meta[name="template"]')?.content;
+let excludeProdPathsIni = [];
+let includeProdPathsIni = [];
+let excludeStagePathsIni = [];
+let includeStagePathsIni = [];
+ 
+if (template === 'pdp') {
 // production paths to go through converter
-export const excludeProdPaths = ['products/family', 'products/sku', 'products/bundle'];
+  excludeProdPathsIni = [];
+ 
+  // include pro paths
+  includeProdPathsIni = ['e-buy', 'products/', 'brands', 'products.html', 'blog-eds', 'news-eds', 'products/product-coveo'];
+ 
+  // exclude stage paths
+  excludeStagePathsIni = ['topics-jck1'];
+ 
+  // include stage paths
+  includeStagePathsIni = ['e-buy', 'products/', 'brands', 'products.html', 'blog', 'news', 'library', 'we-see-a-way', 'products/product-coveo', 'products/family', 'products/sku', 'products/bundle'];
+ 
+}
+else {
+  // production paths to go through converter
+  excludeProdPathsIni = ['products/family', 'products/sku', 'products/bundle'];
+ 
+  // include pro paths
+  includeProdPathsIni = ['e-buy', 'products/', 'brands', 'products.html', 'blog-eds', 'news-eds', 'products/product-coveo'];
+ 
+  // exclude stage paths
+  excludeStagePathsIni = ['products/family', 'products/sku', 'products/bundle', 'topics-jck1'];
+ 
+  // include stage paths
+  includeStagePathsIni = ['e-buy', 'products/', 'brands', 'products.html', 'blog', 'news', 'library', 'we-see-a-way', 'products/product-coveo'];
+}
+ 
+export const excludeProdPaths = excludeProdPathsIni;
+export const includeProdPaths = includeProdPathsIni;
+export const excludeStagePaths = excludeStagePathsIni;
+export const includeStagePaths = includeStagePathsIni;
 
-// include pro paths
-export const includeProdPaths = ['e-buy', 'products/', 'brands', 'products.html', 'blog-eds', 'news-eds', 'products/product-coveo'];
-
-// exclude stage paths
-export const excludeStagePaths = ['products/family', 'products/sku', 'products/bundle', 'topics-jck1'];
-
-// include stage paths
-export const includeStagePaths = ['e-buy', 'products/', 'brands', 'products.html', 'blog', 'news', 'library', 'we-see-a-way', 'products/product-coveo'];
