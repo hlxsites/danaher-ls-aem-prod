3526d1df8a3daeb7546a40947984274184e13d90
APRO2010 <prasad.esarapu@rankuno.com>
2025-10-22 13:57:17 +0530

    moving the release-22102025 files

--- blocks/pdp-carousel/grid-data.js
+++ blocks/pdp-carousel/grid-data.js
@@ -14,7 +14,7 @@ export default function renderGridCard(item) {
   const card = a(
     {
       href: item?.url,
-      target: item?.url?.includes('http') ? '_blank' : '_self',
+      target: item?.url?.includes('https://lifesciences.danaher.com/') ? '_self' : '_blank',
       class:
         'w-full relative flex-shrink-0 object-contain hover:shadow-md transform-origin transform cursor-pointer transition duration-500 sm:min-w-[100%] md:min-w-[305px] sm:w-[calc(50%-10px)] lg:w-[calc(25%-15px)] min-h-80 bg-white border border-gray-300 flex flex-col justify-start items-start gap-3',
     },
@@ -31,10 +31,10 @@ export default function renderGridCard(item) {
       },
       item?.title?.replace(/<[^>]*>/g, '') || '',
     ),
-    span(
+    a(
       {
         href: item?.url,
-        target: item?.url?.includes('http') ? '_blank' : '_self',
+        target: item?.url?.includes('https://lifesciences.danaher.com/') ? '_self' : '_blank',
         class:
           'text-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800 hover:text-danaherpurple-800 !px-3 self-stretch px-3 pb-3 flex justify-start items-center text-base font-bold leading-snug',
       },
