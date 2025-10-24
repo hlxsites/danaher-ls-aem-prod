c303593758dd904cd4d589b4883f140295bbe610
APRO2010 <prasad.esarapu@rankuno.com>
2025-10-24 19:00:23 +0530

    updated the library template logic

--- blocks/card-list/articleCard.js
+++ blocks/card-list/articleCard.js
@@ -15,7 +15,7 @@ export default function createCard(article, firstCard = false) {
 
   // Determine link text based on template and article path
   let linkText = '';
-  if (article.path && article.path.includes('/us/en/videos/')) {
+  if (article.path && article.path.includes('/us/en/videos-eds/')) {
     linkText = 'Watch →';
   } else {
     switch (template) {
