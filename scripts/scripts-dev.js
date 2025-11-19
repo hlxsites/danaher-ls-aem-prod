fbd87c4b6dda53e39d95664d5c049ecc3d8ae025
APRO2010 <prasad.esarapu@rankuno.com>
2025-11-19 20:27:18 +0530

    included Auth0 files for 19November release

--- scripts/scripts-dev.js
+++ scripts/scripts-dev.js
@@ -619,8 +619,8 @@ async function decorateTemplates(main) {
   try {
     let template = toClassName(getMetadata('template'));
     if (template === 'library') {
-    document.body.classList.remove('library');
-    template='blog';
+      document.body.classList.remove('library');
+      template = 'blog';
     }
     const templates = Object.keys(TEMPLATE_LIST);
     if (templates.includes(template)) {
