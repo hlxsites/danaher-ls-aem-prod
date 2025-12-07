3d73da4809f1d3bdb987bc34fec3c0eec4261781
APRO2010 <prasad.esarapu@rankuno.com>
2025-12-07 19:35:55 +0530

    included additional icons in mini teasers block

--- tools/actions/convert/src/converter-paths-config.js
+++ tools/actions/convert/src/converter-paths-config.js
@@ -9,7 +9,7 @@
 export const excludeProdPaths = ['products/family', 'products/sku', 'products/bundle', 'products/product-coveo'];
 
 // include pro paths
-export const includeProdPaths = ['e-buy', 'products/', 'brands', 'products.html', 'blog', 'news', 'videos', 'library', 'expert-eds', 'new-lab', 'application', 'solutions/analytical', 'solutions/digital/products'];
+export const includeProdPaths = ['e-buy', 'products/', 'brands', 'products.html', 'blog', 'news', 'videos', 'library', 'expert-eds', 'new-lab', 'application', 'solutions/analytical', 'solutions/digital/products', 'about-us'];
 
 // exclude stage paths
 export const excludeStagePaths = ['products/family', 'products/sku', 'products/bundle', 'products/product-coveo', 'topics-jck1'];
