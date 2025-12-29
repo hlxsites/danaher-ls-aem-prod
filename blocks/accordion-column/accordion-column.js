be7ee6078086a35bed08af8dcaf24c9cba2a1ea0
APRO2010 <prasad.esarapu@rankuno.com>
2025-12-29 22:01:00 +0530

    added accordion column block

--- blocks/accordion-column/accordion-column.js
+++ blocks/accordion-column/accordion-column.js
@@ -178,11 +178,11 @@ export default async function decorate(block) {
   const dynamicData = Array.from(block.children)
     .slice(1)
     .map((element) => {
-      const paragraphs = element.querySelectorAll('p');
+      const paragraphs = element.querySelectorAll('h3');
       const firstParagraph = paragraphs[0];
       const question = firstParagraph?.textContent.trim() || '';
 
-      element.children[0]?.firstElementChild?.remove();
+      element.children[1]?.firstElementChild?.remove();
       const allChildren = Array.from(element.children);
       element.querySelectorAll('a')?.forEach((aEle) => {
         if (aEle) aEle.classList.add(...'text-sm font-bold text-danaherpurple-500 !no-underline'.split(' '));
@@ -209,7 +209,7 @@ export default async function decorate(block) {
     createdFAQSchema();
 
     const layoutContainer = div({
-      class: 'flex flex-col lg:flex-row gap-x-5 w-full accordion-rendered',
+      class: 'flex flex-col lg:flex-row gap-x-5 w-full accordion-rendered'
     });
     const faqTextContainer = div(
       { class: 'lg:w-[400px]' },
